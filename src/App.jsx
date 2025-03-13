740bae233124ace282c5310de0015c143d649bbe
Roxana Mendoza <mendozasanzcrisro@gmail.com>
2025-03-13 09:49:09 -0600

    Practica_individual

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./assets/Database/authcontext";
+import ProtectedRoute from "./assets/Components/ProtectedRoute"; 
+import Login from './assets/Views/Login'
+import Encabezado from "./assets/Components/Encabezado";
+import Inicio from "./assets/Views/Inicio";
 
-const App = () => {
+import "./App.css";
+
+function App() {
 
   return (
     <>
-      
-        <h1>Componente Principal</h1>
+      <AuthProvider>
+        <Router>
+          <div className="App">
+            <Encabezado />
+            <main>
+              <Routes>
+                
+                <Route path="/" element={<Login />} />
+                <Route path="/inicio" element={<ProtectedRoute element={<Inicio />} />} />
 
+              </Routes>
+            </main>
+          </div>
+        </Router>
+      </AuthProvider>
     </>
   )
-  }
-export default App;
+}
+
+export default App
+
--- src/assets/Database/authcontext.jsx
+++ src/assets/Database/authcontext.jsx
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
 import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
-import { appfirebase } from "./firebaseconfig";
+import { appfirebase } from "./firebaseConfig";
 
 const AuthContext = createContext();
 
