69bdcdd27a6603e3ffc1ac35d926dd4a385ffe51
Roxana Mendoza <mendozasanzcrisro@gmail.com>
2025-03-13 21:23:18 -0600

    estructura 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./database/authcontext";
 import ProtectedRoute from "./components/ProtectedRoute"; 
-import Login from './views/Login'
+import Login from "./views/Login"
 import Encabezado from "./components/Encabezado";
 import Inicio from "./views/Inicio";
 
--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Container } from "react-bootstrap";
-import LoginForm from "../components/LoginForm";
+import LoginForm from "../components/Loginform"
 import { appfirebase } from "../database/firebaseConfig";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { useAuth } from "../database/authcontext";
