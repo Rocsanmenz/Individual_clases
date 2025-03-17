09a003a9a2629d1b6a18c317d5fa0619be938a33
Roxana Mendoza <mendozasanzcrisro@gmail.com>
2025-03-17 10:00:41 -0600

    Practica Individual

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,31 @@ import ProtectedRoute from "./components/ProtectedRoute";
 import Login from "./views/Login"
 import Encabezado from "./components/Encabezado";
 import Inicio from "./views/Inicio";
-
+import Categorias from "./views/categorias";
 import './App.css'
 
 function App() {
 
-  return (
-    <>
-      <AuthProvider>
-        <Router>
-          <div className="App">
-            <Encabezado />
-            <main>
-              <Routes>
-                
-                <Route path="/" element={<Login />} />
-                <Route path="/inicio" element={<ProtectedRoute element={<Inicio />} />} />
+return (
+  <>
+    <AuthProvider>
+      <Router>
+        <div className="App">
+          <Encabezado />
+          <main>
+            <Routes>
+              
+              <Route path="/" element={<Login />} />
+              <Route path="/inicio" element={<ProtectedRoute element={<Inicio />} />} />
+              <Route path="/categorias" element={<ProtectedRoute element={<Categorias />} />}/> //Ruta de Categorias protegida
 
-              </Routes>
-            </main>
-          </div>
-        </Router>
-      </AuthProvider>
-    </>
-  )
+            </Routes>
+          </main>
+        </div>
+      </Router>
+    </AuthProvider>
+  </>
+)
 }
 
 export default App
